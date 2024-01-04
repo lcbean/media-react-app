57385062de181e8c62a231775f259ebd144d4ef7
Lindsay Bean <lcbean@unc.edu>
2024-01-03 19:28:59 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet'></link>
+      <nav class="navbar background">
+        <ul class="nav-list">
+          <div class="logo">
+            <img src="https://i0.wp.com/www.centenarycentre.com/wp-content/uploads/2015/07/movie-film-clip-art-movies.gif?fit=386%2C365&ssl=1"></img>
+          </div>
+          <li>
+            <a href="#login">Login</a>
+          </li>
+          <li>
+            <a href="#ratings">My Ratings</a>
+          </li>
+          <li>
+            <a href="#tbd">Want to See</a>
+          </li>
+        </ul>
+
+        <div class="rightNav">
+          <input
+            type="text"
+            name="search"
+            id="search"
+          />
+          <button class="btn btn-search">
+            Search
+          </button>
+        </div>
+      </nav>
+
+      
     </div>
   );
 }
